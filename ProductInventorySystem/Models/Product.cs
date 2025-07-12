93d0f87fccaef5a5b70d666c9da9647512ac9c83
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-07-12 23:32:27 +0300

    Update data types ranges and limits

--- ProductInventorySystem/Models/Product.cs
+++ ProductInventorySystem/Models/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace ProductInventorySystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductInventorySystem.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [MaxLength(100)]
         public string Name { get; set; }
+
         public int Quantity { get; set; }
-        public double Price { get; set; }
+
+        [Column(TypeName ="decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [MaxLength(50)]
         public string Category { get; set; }
 
 
