fa9ae39ce259b01e9aa16f53e99f73a2efe1b21e
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-07-16 10:55:37 +0300

    Add profiling middleware to evaluate request execution time.

---
+++ ProductInventorySystem/Middlewares/ProfilingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Inventory.Api.Middlewares
+{
+    public class ProfilingMiddleware
+    {
+
+        private readonly ILogger<ProfilingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public ProfilingMiddleware(ILogger<ProfilingMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            await _next(context);
+            stopwatch.Stop();
+
+            var duration = stopwatch.ElapsedMilliseconds;
+
+            _logger.LogInformation($"Request `{context.Request.Path}` took `{duration}ms` to execute");
+        }
+    }
+}
